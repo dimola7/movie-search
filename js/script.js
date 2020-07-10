18e64b86e41e10eef0bf5e3d5f9a0e2a8d2aa4d5
Dimola Ogidan <didiogidan@gmail.com>
2020-07-10 13:15:16 +0100

    redesigned header

--- js/script.js
+++ js/script.js
@@ -6,6 +6,20 @@ const title = document.querySelector(".movie-title");
 const upcoming = document.querySelector(".upcoming");
 const shows = document.querySelector(".shows");
 const topRated = document.querySelector(".top-rated");
+const logo = document.querySelector(".logo");
+const card = document.querySelector(".header-card");
+const form = document.querySelector(".search");
+
+const tl = new TimelineMax();
+
+tl.fromTo(logo, 1, { height: "80%" }, { height: "5%", ease: Power2.easeInOut })
+  .fromTo(form, 0.4, { top: "-15%" }, { top: "5%", ease: Power2.easeInOut })
+  .fromTo(
+    card,
+    0.4,
+    { maxHeight: "0%" },
+    { maxHeight: "50%", ease: Power2.easeInOut }
+  );
 
 //api doc instructs to add this url before every image url
 const IMG_URL = "https://image.tmdb.org/t/p/w500";
@@ -202,3 +216,91 @@ if (localStorage.getItem("movies")) {
     .then((data) => updateUI(data))
     .catch((err) => console.log(err));
 }
+
+const slides = document.querySelector(".slider").children;
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+const indicator = document.querySelector(".indicator");
+let index = 0;
+
+prev.addEventListener("click", function () {
+  prevSlide();
+  updateCircleIndicator();
+  resetTimer();
+});
+
+next.addEventListener("click", function () {
+  nextSlide();
+  updateCircleIndicator();
+  resetTimer();
+});
+
+// create circle indicators
+function circleIndicator() {
+  for (let i = 0; i < slides.length; i++) {
+    const div = document.createElement("div");
+    div.innerHTML = i + 1;
+    div.setAttribute("onclick", "indicateSlide(this)");
+    div.id = i;
+    if (i == 0) {
+      div.className = "active";
+    }
+    indicator.appendChild(div);
+  }
+}
+circleIndicator();
+
+function indicateSlide(element) {
+  index = element.id;
+  changeSlide();
+  updateCircleIndicator();
+  resetTimer();
+}
+
+function updateCircleIndicator() {
+  for (let i = 0; i < indicator.children.length; i++) {
+    indicator.children[i].classList.remove("active");
+  }
+  indicator.children[index].classList.add("active");
+}
+
+function prevSlide() {
+  if (index == 0) {
+    index = slides.length - 1;
+  } else {
+    index--;
+  }
+  changeSlide();
+}
+
+function nextSlide() {
+  if (index == slides.length - 1) {
+    index = 0;
+  } else {
+    index++;
+  }
+  changeSlide();
+}
+
+function changeSlide() {
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("active");
+  }
+
+  slides[index].classList.add("active");
+}
+
+function resetTimer() {
+  // when click to indicator or controls button
+  // stop timer
+  clearInterval(timer);
+  // then started again timer
+  timer = setInterval(autoPlay, 4000);
+}
+
+function autoPlay() {
+  nextSlide();
+  updateCircleIndicator();
+}
+
+let timer = setInterval(autoPlay, 4000);
