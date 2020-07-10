f60a93ad044146706d88806f3158ffcc1e9f8a74
Dimola Ogidan <didiogidan@gmail.com>
2020-07-10 15:20:07 +0100

    added spacing in texts, changed buttons font color, added transition to images, refactored javascript

--- js/script.js
+++ js/script.js
@@ -9,7 +9,12 @@ const topRated = document.querySelector(".top-rated");
 const logo = document.querySelector(".logo");
 const card = document.querySelector(".header-card");
 const form = document.querySelector(".search");
+const slides = document.querySelector(".slider").children;
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+const indicator = document.querySelector(".indicator");
 
+//gsap animation for logo and search bar to slide down
 const tl = new TimelineMax();
 
 tl.fromTo(logo, 1, { height: "80%" }, { height: "5%", ease: Power2.easeInOut })
@@ -217,26 +222,10 @@ if (localStorage.getItem("movies")) {
     .catch((err) => console.log(err));
 }
 
-const slides = document.querySelector(".slider").children;
-const prev = document.querySelector(".prev");
-const next = document.querySelector(".next");
-const indicator = document.querySelector(".indicator");
 let index = 0;
 
-prev.addEventListener("click", function () {
-  prevSlide();
-  updateCircleIndicator();
-  resetTimer();
-});
-
-next.addEventListener("click", function () {
-  nextSlide();
-  updateCircleIndicator();
-  resetTimer();
-});
-
 // create circle indicators
-function circleIndicator() {
+const circleIndicator = () => {
   for (let i = 0; i < slides.length; i++) {
     const div = document.createElement("div");
     div.innerHTML = i + 1;
@@ -247,60 +236,59 @@ function circleIndicator() {
     }
     indicator.appendChild(div);
   }
-}
+};
 circleIndicator();
 
-function indicateSlide(element) {
+const indicateSlide = (element) => {
   index = element.id;
   changeSlide();
   updateCircleIndicator();
   resetTimer();
-}
+};
 
-function updateCircleIndicator() {
+const updateCircleIndicator = () => {
   for (let i = 0; i < indicator.children.length; i++) {
     indicator.children[i].classList.remove("active");
   }
   indicator.children[index].classList.add("active");
-}
+};
 
-function prevSlide() {
+const prevSlide = () => {
   if (index == 0) {
     index = slides.length - 1;
   } else {
     index--;
   }
   changeSlide();
-}
+};
 
-function nextSlide() {
+const nextSlide = () => {
   if (index == slides.length - 1) {
     index = 0;
   } else {
     index++;
   }
   changeSlide();
-}
+};
 
-function changeSlide() {
+const changeSlide = () => {
   for (let i = 0; i < slides.length; i++) {
     slides[i].classList.remove("active");
   }
 
   slides[index].classList.add("active");
-}
+};
 
-function resetTimer() {
-  // when click to indicator or controls button
+const resetTimer = () => {
   // stop timer
   clearInterval(timer);
   // then started again timer
   timer = setInterval(autoPlay, 4000);
-}
+};
 
-function autoPlay() {
+const autoPlay = () => {
   nextSlide();
   updateCircleIndicator();
-}
+};
 
 let timer = setInterval(autoPlay, 4000);
